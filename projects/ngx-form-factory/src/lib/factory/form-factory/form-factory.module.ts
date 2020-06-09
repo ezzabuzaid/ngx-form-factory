d47f3b6760aab51af3869dc4fcff0dd5a443ffbe
ezzabuzaid <ezzabuzaid@hotmail.com>
2020-06-09 15:41:27 +0300

    fix: import material button and icon modules, strech field width to 100%

--- projects/ngx-form-factory/src/lib/factory/form-factory/form-factory.module.ts
+++ projects/ngx-form-factory/src/lib/factory/form-factory/form-factory.module.ts
@@ -5,6 +5,8 @@ import { FormFactoryComponent } from './form-factory.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { FieldFactoryModule } from '../field-factory/field-factory.module';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
@@ -17,6 +19,8 @@ import { FieldFactoryModule } from '../field-factory/field-factory.module';
         MatCardModule,
         MatProgressBarModule,
         FieldFactoryModule,
+        MatButtonModule,
+        MatIconModule
     ],
     exports: [
         FormFactoryComponent
