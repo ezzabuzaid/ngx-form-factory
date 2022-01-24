d8845a5a9213438da3302a33d6bc17753586bc8d
ezzabuzaid <Ezzabuzaid@hotmail.com>
2022-01-24 22:39:45 +0200

    fix(SelectOption): use value as option value instead of label
    BREAKING CHANGE: changing the position of value parameter and change key property to label

--- projects/ngx-form-factory/src/lib/fields/select_option.ts
+++ projects/ngx-form-factory/src/lib/fields/select_option.ts
@@ -2,23 +2,23 @@ import { isNullorUndefined } from '../shared';
 
 /**
  * The mat-select `<mat-option></mat-option>`
- * 
+ *
  * Warning: value represent the label and the key represent the value
- * 
+ *
  */
 export class SelectOption {
     constructor(
         /**
-         * The Label that will be shown in the select option
-         */
-        public value: string,
-        /**
          * The value of the option that will be used as field value
          */
-        public key?: string | number
+        public value: string | number | boolean,
+        /**
+         * The Label that will be shown in the select option
+         */
+        public label?: string,
     ) {
-        if (isNullorUndefined(this.key)) {
-            this.key = this.value;
+        if (isNullorUndefined(this.label)) {
+            this.label = `${this.value}`;
         }
     }
 }
