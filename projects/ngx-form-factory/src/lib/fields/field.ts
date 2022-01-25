5fcd5135097d0fba896e320cce8f5d236ee8e1ec
ezzabuzaid <Ezzabuzaid@hotmail.com>
2022-01-25 17:04:06 +0200

    feat: add appearance, floatLabel, and hideRequiredMarker as field options

--- projects/ngx-form-factory/src/lib/fields/field.ts
+++ projects/ngx-form-factory/src/lib/fields/field.ts
@@ -1,9 +1,10 @@
+import { FloatLabelType, MatFormFieldAppearance, MatFormFieldDefaultOptions } from '@angular/material/form-field';
 import { BaseField, EFieldType, IBaseField, IBaseFieldOptions } from './base';
 
 
-export interface IFieldOptions<T> extends IBaseFieldOptions<T> {
+export interface IFieldOptions<T> extends IBaseFieldOptions<T>, MatFormFieldDefaultOptions {
     /**
-     * Field placeholder 
+     * Field placeholder
      */
     label?: string;
     /**
@@ -12,19 +13,25 @@ export interface IFieldOptions<T> extends IBaseFieldOptions<T> {
     hint?: string;
 }
 
-export interface IField<T> extends IBaseField<T> {
+export interface IField<T> extends IBaseField<T>, MatFormFieldDefaultOptions {
     label?: string;
     hint?: string;
 }
 
-export class Field<T> extends BaseField<T> implements IField<T> {
+export class Field<T> extends BaseField<T> implements IField<T>{
     public label?: string;
     public hint?: string;
+    public appearance?: MatFormFieldAppearance;
+    public floatLabel?: FloatLabelType;
+    public hideRequiredMarker?: boolean;
     constructor(options?: IFieldOptions<T>) {
         super(options);
         this.type = options?.type ?? EFieldType.TEXT;
         this.label = options?.label;
         this.hint = options?.hint;
+        this.appearance = options?.appearance;
+        this.floatLabel = options?.floatLabel;
+        this.hideRequiredMarker = options?.hideRequiredMarker;
     }
 
 }
