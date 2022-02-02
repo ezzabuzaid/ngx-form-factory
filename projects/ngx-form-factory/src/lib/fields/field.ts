e7c75a4a5f7fec66ebfc5461c769c442f0c6eace
ezzabuzaid <Ezzabuzaid@hotmail.com>
2022-02-02 23:16:12 +0200

    feat: add placeholder, readonly, maxlength, and minlength to all fields

--- projects/ngx-form-factory/src/lib/fields/field.ts
+++ projects/ngx-form-factory/src/lib/fields/field.ts
@@ -3,19 +3,30 @@ import { BaseField, EFieldType, IBaseField, IBaseFieldOptions } from './base';
 
 
 export interface IFieldOptions<T> extends IBaseFieldOptions<T>, MatFormFieldDefaultOptions {
+    maxlength?: number;
+    minlength?: number;
+    readonly?: boolean;
     /**
      * Field placeholder
      */
+    placeholder?: string;
+    /**
+     * Field label
+     */
     label?: string;
     /**
-     * small text to show underneath the field
+     * Small text to show underneath the field
      */
     hint?: string;
 }
 
 export interface IField<T> extends IBaseField<T>, MatFormFieldDefaultOptions {
+    maxlength?: number;
+    minlength?: number;
+    readonly?: boolean;
     label?: string;
     hint?: string;
+    placeholder?: string;
 }
 
 export class Field<T> extends BaseField<T> implements IField<T>{
@@ -24,6 +35,10 @@ export class Field<T> extends BaseField<T> implements IField<T>{
     public appearance?: MatFormFieldAppearance;
     public floatLabel?: FloatLabelType;
     public hideRequiredMarker?: boolean;
+    public readonly?: boolean;
+    public maxlength?: number;
+    public minlength?: number;
+    public placeholder?: string;
     constructor(options?: IFieldOptions<T>) {
         super(options);
         this.type = options?.type ?? EFieldType.TEXT;
@@ -32,6 +47,10 @@ export class Field<T> extends BaseField<T> implements IField<T>{
         this.appearance = options?.appearance;
         this.floatLabel = options?.floatLabel;
         this.hideRequiredMarker = options?.hideRequiredMarker;
+        this.placeholder = options?.placeholder;
+        this.minlength = options?.minlength;
+        this.maxlength = options?.maxlength;
+        this.readonly = options?.readonly;
     }
 
 }
