bc495d48d76923f0b49c2a8601092ce9bfa69c08
ezzabuzaid <Ezzabuzaid@hotmail.com>
2022-02-03 02:02:39 +0200

    refactor: group submit button inputs under object options
    BREAKING CHANGE: this will affect your codebase

--- projects/ngx-form-factory/src/lib/factory/form-factory/form-factory.component.ts
+++ projects/ngx-form-factory/src/lib/factory/form-factory/form-factory.component.ts
@@ -1,98 +1,110 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, HostBinding, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, EventEmitter, HostBinding, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges } from '@angular/core';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { Form, IBaseField, TFields } from '../../fields/base';
 import { assertNotNullOrUndefined } from '../../shared';
 import { FormFactoryManager } from '../form-factory.manager';
+import { SubmitButtonOptions } from './submit_button_options';
 import { SubmitEvent } from './submit_event';
 
 @Component({
-  selector: 'ngx-form-factory',
-  templateUrl: './form-factory.component.html',
-  styleUrls: ['./form-factory.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'ngx-form-factory',
+    templateUrl: './form-factory.component.html',
+    styleUrls: ['./form-factory.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class FormFactoryComponent implements OnInit, OnDestroy {
-  public readonly progressListener = this.formWidgetManager.watch();
-  private readonly subscription = new Subject();
-  @Input() submitButtonDisableState = false;
-  @Output() public readonly onSubmit = new EventEmitter<SubmitEvent>();
-  @Input() public title?: string;
-  @Input() formGroup!: Form<any>;
-  @Input() submitButton = true;
-  @Input() implicitFields = true;
-  @Input() autoValidateSubmitButton = true;
-  @HostBinding('class.loading') public loading = false;
+export class FormFactoryComponent implements OnInit, OnDestroy, OnChanges {
+    public readonly progressListener = this.formWidgetManager.watch();
+    private readonly subscription = new Subject();
+    @Input() submitButtonOptions?: SubmitButtonOptions;
+    @Output() public readonly onSubmit = new EventEmitter<SubmitEvent>();
+    @Input() formGroup!: Form<any>;
+    @Input() implicitFields = true;
+    @HostBinding('class.loading') public loading = false;
 
-  sectionsNames: string[] = [];
-  sections!: { [key: string]: IBaseField<any>[] };
+    sectionsNames: string[] = [];
+    sections!: { [key: string]: IBaseField<any>[] };
 
-  constructor(
-    private readonly formWidgetManager: FormFactoryManager,
-  ) { }
+    constructor(
+        private readonly formWidgetManager: FormFactoryManager,
+        private cdf: ChangeDetectorRef
+    ) { }
 
-  ngOnInit() {
-    assertNotNullOrUndefined(this.formGroup);
+    ngOnChanges(changes: SimpleChanges): void {
+        if (changes['submitButtonOptions']) {
+            Object.assign({}, {
+                autoValidate: true,
+                disabled: false,
+                show: true,
+                class: []
+            } as SubmitButtonOptions, this.submitButtonOptions)
+        }
+    }
 
-    this.progressListener
-      .pipe(takeUntil(this.subscription))
-      .subscribe(show => {
-        this.loading = show;
-      });
+    ngOnInit() {
+        assertNotNullOrUndefined(this.formGroup);
 
-    const fields = this.flattenFields(values(this.formGroup.fields));
-    this.sectionsNames = [...new Set(fields.map(field => field.section))];
-    this.sections = this.groupBySection(fields);
-  }
+        this.progressListener
+            .pipe(takeUntil(this.subscription))
+            .subscribe(show => {
+                this.loading = show;
+            });
 
-  submit() {
-    this.onSubmit.emit({
-      value: this.formGroup.value,
-      valid: this.formGroup.valid
-    });
-  }
+        const fields = this.flattenFields(values(this.formGroup.fields));
+        this.sectionsNames = [...new Set(fields.map(field => field.section))];
+        this.sections = this.groupBySection(fields);
+    }
 
-  ngOnDestroy() {
-    this.subscription.next(null);
-    this.subscription.complete();
-  }
+    submit() {
+        this.onSubmit.emit({
+            value: this.formGroup.value,
+            valid: this.formGroup.valid
+        });
+    }
 
-  form(field: any) {
-    return field instanceof Form ? field : null;
-  }
+    ngOnDestroy() {
+        this.subscription.next(null);
+        this.subscription.complete();
+    }
 
-  private flattenFields(controls: TFields<any>[0][]) {
-    return controls.reduce((acc, control) => {
-      if (this.form(control)) {
-        acc.push(...this.flattenFields(values(this.form(control)!.fields)));
-      } else {
-        acc.push(control);
-      }
-      return acc;
-    }, [] as any[]);
-  }
+    form(field: any) {
+        return field instanceof Form ? field : null;
+    }
 
-  private groupBySection(fields: IBaseField<any>[]) {
-    return fields.reduce((acc, curr) => {
-      const section = curr.section as unknown as string;
-      if (!acc[section]) {
-        acc[section] = [];
-      }
-      acc[section].push(curr);
-      return acc;
-    }, {} as Record<string, any[]>);
-  }
+    private flattenFields(controls: TFields<any>[0][]) {
+        return controls.reduce((acc, control) => {
+            if (this.form(control)) {
+                acc.push(...this.flattenFields(values(this.form(control)!.fields)));
+            } else {
+                acc.push(control);
+            }
+            return acc;
+        }, [] as any[]);
+    }
 
-  disableSubmitButton() {
-    this.submitButtonDisableState = true;
-  }
+    private groupBySection(fields: IBaseField<any>[]) {
+        return fields.reduce((acc, curr) => {
+            const section = curr.section as unknown as string;
+            if (!acc[section]) {
+                acc[section] = [];
+            }
+            acc[section].push(curr);
+            return acc;
+        }, {} as Record<string, any[]>);
+    }
 
-  enableSubmitButton() {
-    this.submitButtonDisableState = false;
-  }
+    disableSubmitButton() {
+        this.submitButtonOptions.disabled = true;
+        this.cdf.markForCheck();
+    }
+
+    enableSubmitButton() {
+        this.submitButtonOptions.disabled = false;
+        this.cdf.markForCheck();
+    }
 
 }
 
 function values(fields: TFields<any>): TFields<any>[0][] {
-  return Object.values(fields);
+    return Object.values(fields);
 }
--- projects/ngx-form-factory/src/lib/factory/index.ts
+++ projects/ngx-form-factory/src/lib/factory/index.ts
@@ -2,5 +2,6 @@ export * from './factory.module';
 export * from './field-factory/field-factory.component';
 export * from './form-factory.manager';
 export * from './form-factory/form-factory.component';
+export * from './form-factory/submit_button_options';
 export * from './form-factory/submit_event';
 
