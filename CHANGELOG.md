578c51fd05122c300ffbe7a96146ff02028525a4
ezzabuzaid <Ezzabuzaid@hotmail.com>
2022-02-13 23:53:02 +0200

    fix(DateField): pass DatePicker instance to monthSelected and yearSelected

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,10 @@
+### [1.1.1](https://github.com/ezzabuzaid/ngx-form-factory/compare/v1.1.0...v1.1.1) (2022-02-13)
+
+
+### Bug Fixes
+
+* **DateField:** pass  to monthSelected and yearSelected ([7821c59](https://github.com/ezzabuzaid/ngx-form-factory/commit/7821c5955fac8a13c74ebf0b104bf0daba827954))
+
 ## [1.1.0](https://github.com/ezzabuzaid/ngx-form-factory/compare/v1.0.4...v1.1.0) (2022-02-13)
 
 
--- projects/ngx-form-factory/src/lib/fields/date.ts
+++ projects/ngx-form-factory/src/lib/fields/date.ts
@@ -1,21 +1,22 @@
+import { MatDatepicker } from '@angular/material/datepicker';
 import { MatDatepickerBase } from '@angular/material/datepicker/datepicker-base';
 import { Field, IFieldOptions } from './field';
 import { EFieldType } from './field_type';
 
-export interface IDateField {
+export interface IDateField<PT> {
     min?: Date;
     max?: Date;
     startView?: MatDatepickerBase<any, any, Date>['startView'];
     startAt?: MatDatepickerBase<any, any, Date>['startAt'];
-    yearSelected?: (date: Date) => void;
-    monthSelected?: (date: Date) => void;
+    yearSelected?: (date: Date, picker: MatDatepicker<PT>) => void;
+    monthSelected?: (date: Date, picker: MatDatepicker<PT>) => void;
 }
 
-export interface IDateFieldOptions extends IFieldOptions<Date> {
+export interface IDateFieldOptions<PT> extends IFieldOptions<Date> {
     startView?: MatDatepickerBase<any, any, Date>['startView'];
     startAt?: MatDatepickerBase<any, any, Date>['startAt'];
-    yearSelected?: (date: Date) => void;
-    monthSelected?: (date: Date) => void;
+    yearSelected?: (date: Date, picker: MatDatepicker<PT>) => void;
+    monthSelected?: (date: Date, picker: MatDatepicker<PT>) => void;
     /**
      * Minumum allowed date to enter
      *
@@ -29,21 +30,21 @@ export interface IDateFieldOptions extends IFieldOptions<Date> {
     */
     max?: Date;
 }
-export class DateField extends Field<Date> implements IDateField {
+export class DateField<PT = Date> extends Field<Date> implements IDateField<PT> {
     public min?: Date;
     public max?: Date;
     public startView?: MatDatepickerBase<any, any, Date>['startView'];
     public startAt?: MatDatepickerBase<any, any, Date>['startAt'];
-    public yearSelected?: (date: Date) => void;
-    public monthSelected?: (date: Date) => void;
-    constructor(options?: IDateFieldOptions) {
+    public yearSelected?: (date: Date, picker: MatDatepicker<PT>) => void;
+    public monthSelected?: (date: Date, picker: MatDatepicker<PT>) => void;
+    constructor(options?: IDateFieldOptions<PT>) {
         super(options);
         this.type = EFieldType.DATE;
         this.min = options?.min;
         this.max = options?.max;
         this.startAt = options?.startAt;
-        this.startView = options?.startView;
-        this.yearSelected = options?.yearSelected ?? ((d) => { });
-        this.monthSelected = options?.monthSelected ?? ((d) => { });
+        this.startView = options?.startView ?? 'month';
+        this.yearSelected = options?.yearSelected ?? ((d, p) => { });
+        this.monthSelected = options?.monthSelected ?? ((d, p) => { });
     }
 }
