bca8c60ccd0dc0ca339427ad1293ad30bfc67639
ezzabuzaid <Ezzabuzaid@hotmail.com>
2023-02-02 23:10:01 +0300

    fix(MaskField): remove default value for mask

--- libs/ngx-form-factory/src/lib/fields/mask.ts
+++ libs/ngx-form-factory/src/lib/fields/mask.ts
@@ -1,5 +1,5 @@
 import { MatFormFieldDefaultOptions } from '@angular/material/form-field';
-import { createMask, InputmaskOptions } from '@ngneat/input-mask';
+import { InputmaskOptions } from '@ngneat/input-mask';
 
 import { Field, IField, IFieldOptions, ITextOptions } from './field';
 import { EFieldType } from './field_type';
@@ -8,27 +8,18 @@ export interface IMaskFieldOptions<T>
   extends IFieldOptions<T>,
     MatFormFieldDefaultOptions,
     ITextOptions {
-  mask?: InputmaskOptions<T>;
+  mask: InputmaskOptions<T>;
 }
 
 export interface IMaskField<T> extends IField<T> {
-  mask?: InputmaskOptions<T>;
+  mask: InputmaskOptions<T>;
 }
 
 export class MaskField<T> extends Field<T> implements IMaskField<T> {
   mask: InputmaskOptions<T>;
-  constructor(options?: IMaskFieldOptions<T>) {
+  constructor(options: IMaskFieldOptions<T>) {
     super(options);
     this.type = EFieldType.MASK;
-    this.mask =
-      options?.mask ??
-      createMask({
-        alias: 'numeric',
-        groupSeparator: ',',
-        digits: 2,
-        digitsOptional: false,
-        prefix: '$ ',
-        placeholder: options?.placeholder,
-      });
+    this.mask = options.mask;
   }
 }
