202dc3f61997ab10ec6961955db93ea2b1e3ea6c
ezzabuzaid <Ezzabuzaid@hotmail.com>
2023-06-18 00:03:25 +0300

    feat(RawField): add ability to pass injector

--- libs/ngx-form-factory/src/lib/fields/raw.ts
+++ libs/ngx-form-factory/src/lib/fields/raw.ts
@@ -1,4 +1,4 @@
-import { Type } from '@angular/core';
+import { Injector, Type } from '@angular/core';
 import { ControlValueAccessor } from '@angular/forms';
 
 import { UserInputs, UserOutputs } from '../shared';
@@ -8,7 +8,7 @@ import { EFieldType } from './field_type';
 export interface IRawFieldOptions<T>
   extends Omit<IBaseFieldOptions<T>, 'type' | 'errorsMessages'> {
   /**
-   * the component which will act as field
+   * The component which will act as field
    */
   component: Type<ControlValueAccessor>;
   /**
@@ -21,12 +21,18 @@ export interface IRawFieldOptions<T>
   outputs?: {
     [key: string]: (event: any) => any;
   };
+
+  /**
+   * Injector to use for component instantiation.
+   */
+  injector?: Injector;
 }
 
 export class RawField<T, C> extends BaseField<T> {
   component: Type<ControlValueAccessor>;
   inputs?: UserInputs;
   outputs?: UserOutputs;
+  injector?: Injector;
   componentInstance!: C;
   constructor(options: IRawFieldOptions<T>) {
     super(options);
@@ -34,5 +40,6 @@ export class RawField<T, C> extends BaseField<T> {
     this.component = options.component;
     this.inputs = options?.inputs;
     this.outputs = options?.outputs;
+    this.injector = options?.injector;
   }
 }
